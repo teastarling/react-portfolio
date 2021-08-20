111d4c95f03adfadbb20b0ccbfa1550f8dc43874
Tyler Starling <tyler.a.starling@gmail.com>
2021-08-19 20:34:49 -0400

    Updates

---
+++ static/js/main.68fa0aeb.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-portfolio"]=this["webpackJsonpreact-portfolio"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));function a(){return Object(s.jsx)("main",{children:Object(s.jsx)("h1",{children:"Hello"})})}i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.68fa0aeb.chunk.js.map
